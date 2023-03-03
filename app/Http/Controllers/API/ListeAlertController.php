1763cbfc355a23cb48b2e3d9791565b54cc34240
fiflokonon <fiflokonon@gmail.com>
2023-03-03 21:41:39 +0100

    notif

--- app/Http/Controllers/API/ListeAlertController.php
+++ app/Http/Controllers/API/ListeAlertController.php
@@ -6,6 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Alert;
 use Illuminate\Support\Facades\DB;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging\AndroidConfig;
+use Kreait\Firebase\Messaging\ApnsConfig;
+use Kreait\Firebase\Messaging\MulticastMessage;
+use Kreait\Firebase\Messaging\Notification;
+use Kreait\Firebase\Messaging\WebpushConfig;
+use Kreait\Firebase\ServiceAccount;
+
 
 class ListeAlertController extends Controller
 
@@ -56,4 +64,33 @@ class ListeAlertController extends Controller
 
         return $alerts;
     }
+
+
+
+    public function sendFirebaseNotification($topic, $title, $body, $data = []) {
+    // Initialiser Firebase Messaging avec la clé privée
+    $serviceAccount = ServiceAccount::fromJsonFile($serviceAccount = ServiceAccount::fromJsonFile(storage_path('hackathon-e6f1f-firebase-adminsdk-kni0l-daf48d7f11.json')));
+    $firebase = (new Factory)->withServiceAccount($serviceAccount)->create();
+
+    // Créer l'objet de notification
+    $notification = Notification::create($title, $body);
+
+    // Créer les configurations pour Android, APNS et Webpush
+    /*$android = AndroidConfig::fromArray($androidConfig);
+    $apns = ApnsConfig::fromArray($apnsConfig);
+    $webpush = WebpushConfig::fromArray($webpushConfig);*/
+
+    // Créer l'objet de message multicast pour envoyer des notifications à tous les périphériques inscrits au topic
+    $multicastMessage = MulticastMessage::fromArray([
+        'topic' => $topic,
+        'notification' => $notification,
+        'android' => $android,
+        'data' => $data
+    ]);
+
+    // Envoyer le message multicast
+    $firebase->getMessaging()->sendMulticast($multicastMessage);
+}
+
+    
 }
